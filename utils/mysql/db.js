720d5392daef6643e594504bc5e834632c64fc90
evensun <evensun@tencent.com>
2021-04-03 22:50:00 +0800

    fix: 回复远端数据库密码

--- utils/mysql/db.js
+++ utils/mysql/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: 'gm2466GMR',
+  password: '@gm2466GMR',
   database: 'welearn'
 });
 
